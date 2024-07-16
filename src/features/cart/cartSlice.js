9e459b32e2c3f825a8eb6d88feb51f2cd6a8c5d3
Farideh <heidarian89@gmail.com>
2024-07-16 07:20:20 +0330

    Implement addToCart reducer in cartSlice.js

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,4 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { cartItems: [], total: 0 };
 
-const cartSlice = createSlice({ name: "cart",initialState,reducers:{} });
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const product = action.payload;
+      const exist = state.cartItems.find((x) => x.id === product.id);
+      if (exist) {
+        exist.qty += 1;
+      } else {
+        state.cartItems.push({ ...product, qty: 1 });
+      }
+    },
+  },
+});
