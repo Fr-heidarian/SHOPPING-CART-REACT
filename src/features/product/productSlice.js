778ba65847ed53d29a5d1cdf64160b97fd08609c
Farideh <heidarian89@gmail.com>
2024-07-20 18:46:05 +0330

    Implement readProducts function in productSlice - fetch data from server

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,6 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = { products: [], loading: false, error: "" };
-export const productSlice = createSlice({ name: "product", initialState });
 
+export const readProducts = createAsyncThunk("product/readProducts", () => {
+  return fetch("https://6300a18859a8760a757d441c.mockapi.io/User")
+    .then((res) => res.json())
+    .catch((e) => e.message);
+});
+
+export const productSlice = createSlice({ name: "product", initialState });
 export default productSlice.reducer;
