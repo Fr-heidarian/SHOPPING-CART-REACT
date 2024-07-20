a59fbeb77d7e14bcae201aa08d0332ee5d4c3c3a
Farideh <heidarian89@gmail.com>
2024-07-20 19:02:22 +0330

    Add extraReducers in productSlice

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -8,5 +8,22 @@ export const readProducts = createAsyncThunk("product/readProducts", () => {
     .catch((e) => e.message);
 });
 
-export const productSlice = createSlice({ name: "product", initialState });
+export const productSlice = createSlice({
+  name: "product",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(readProducts.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(readProducts.fulfilled, (state, action) => {
+      state.loading = false;
+      state.products = action.payload;
+    });
+    builder.addCase(readProducts.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.products = [];
+    });
+  },
+});
 export default productSlice.reducer;
