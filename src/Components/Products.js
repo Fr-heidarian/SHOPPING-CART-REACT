ce9c993149444bc5389ed7e26a2c904c49f96ab1
Farideh <heidarian89@gmail.com>
2024-07-21 14:47:02 +0330

    update products.js component using Redux Toolkit

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,79 +1,19 @@
-import { useEffect, useReducer } from "react";
+import { useEffect } from "react";
 import Product from "./Product";
 import LoadingComponent from "./LoadingComponent";
-
-const ACTIONS = {
-  FETCH_REQUEST: "fetch_request",
-  FETCH_SUCCESS: "fetch_success",
-  FETCH_FAIL: "fetch_fail",
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTIONS.FETCH_REQUEST:
-      return { ...state, loading: true };
-    case ACTIONS.FETCH_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        error: "",
-        products: action.payload ?? [],
-      };
-    case ACTIONS.FETCH_FAIL:
-      return { ...state, loading: false, error: action.payload, products: [] };
-    default:
-      return state;
-  }
-};
+import { useDispatch, useSelector } from "react-redux";
+import { readProducts } from "../features/product/productSlice";
 
 export default function Products({ searchParam }) {
-  // const [products, setProducts] = useState([]);
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState("");
-
-  const [{ products, loading, error }, dispatch] = useReducer(reducer, {
-    products: [],
-    loading: false,
-    error: "",
-  });
+  const dispatch = useDispatch();
+  const { loading, products, error } = useSelector((state) => state.product);
 
   useEffect(() => {
-    const readProducts = async () => {
-      let url = "https://6300a18859a8760a757d441c.mockapi.io/User";
-      if (searchParam) {
-        url += `?name=${searchParam}`;
-      }
-      try {
-        // setLoading(true);
-        dispatch({ type: ACTIONS.FETCH_REQUEST });
-        const response = await fetch(url);
-        if (response.status === 200) {
-          const products = await response.json();
-          // setProducts(products);
-          dispatch({ type: ACTIONS.FETCH_SUCCESS, payload: products });
-        } else {
-          // setProducts([]);
-          // setError(`Server responsed with error code ${response.status}`);
-          dispatch({
-            type: ACTIONS.FETCH_FAIL,
-            payload: `Server responsed with error code ${response.status}`,
-          });
-        }
-      } catch (e) {
-        console.log(e);
-        // setError(e.message);
-        dispatch({ type: ACTIONS.FETCH_FAIL, payload: e.message });
-      }
-      //  finally {
-      //   setLoading(false);
-      // }
-    };
-
     let timerId;
     if (searchParam) {
-      timerId = setTimeout(readProducts, 1000);
+      timerId = setTimeout(() => dispatch(readProducts(searchParam)), 1000);
     } else {
-      readProducts();
+      dispatch(readProducts());
     }
 
     return () => clearTimeout(timerId);
--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -2,11 +2,25 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = { products: [], loading: false, error: "" };
 
-export const readProducts = createAsyncThunk("product/readProducts", () => {
-  return fetch("https://6300a18859a8760a757d441c.mockapi.io/User")
-    .then((res) => res.json())
-    .catch((e) => e.message);
-});
+export const readProducts = createAsyncThunk(
+  "product/readProducts",
+  async (searchParam, { rejectWithValue }) => {
+    try {
+      const res = await fetch(
+        `https://6300a18859a8760a757d441c.mockapi.io/User?name=${
+          searchParam ?? ""
+        }`
+      );
+      if (res.status === 404) {
+        return rejectWithValue("Not Found");
+      } else {
+        return await res.json();
+      }
+    } catch (e) {
+      return e.message;
+    }
+  }
+);
 
 export const productSlice = createSlice({
   name: "product",
