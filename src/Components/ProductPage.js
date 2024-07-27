1f7ee3ec4c89320ca6b58e08ce669b4fcd7325cb
Farideh <heidarian89@gmail.com>
2024-07-27 04:58:06 +0330

    Implement useReadProductQuery and useParams in ProductPage for fetching individual product

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,3 +1,13 @@
+import { useParams } from "react-router-dom";
+import { useReadProductQuery } from "../features/product/productSlice-rtkQuery";
+import LoadingComponent from "./LoadingComponent";
+
 export default function ProductPage() {
-  return <div>ProductPage</div>;
+  const { productId } = useParams();
+  const { data: product, isLoading } = useReadProductQuery(productId);
+
+  if (isLoading) {
+    return <LoadingComponent />;
+  }
+  return <div>{product.name}</div>;
 }
