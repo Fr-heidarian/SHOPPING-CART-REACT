ff8f2d33eaf9e59fe25a0df67da4399b1a4a830b
Farideh <heidarian89@gmail.com>
2024-07-30 12:30:37 +0330

    add create new product Form

--- src/pages/CreateProductPage.js
+++ src/pages/CreateProductPage.js
@@ -1,8 +1,40 @@
+import { useState } from "react";
 
 export default function CreateProductPage() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-     Create New Product Page
+      <form onSubmit={handlesubmit} className="form-container">
+        <label className="form-label">
+          Name :
+          <input
+            type="text"
+            id="name"
+            className="form-input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label className="form-label">
+          Price :
+          <input
+            type="number"
+            id="price"
+            className="form-input"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </label>
+        <br />
+        <input type="submit" className="submit-button" value={"CREATE"} />
+      </form>
     </div>
   );
 }
